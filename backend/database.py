c3b702c1bc6b7b6ab2122b6fb941d1ba43e23edb
Damanpreet Singh Walia <damanpreet3582@gmail.com>
2026-02-11 13:28:42 +0530

    resolve error

--- backend/database.py
+++ backend/database.py
@@ -18,3 +18,11 @@ engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
